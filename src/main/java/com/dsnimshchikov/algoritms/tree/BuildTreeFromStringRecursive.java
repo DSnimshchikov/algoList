bc4cac040487e96bdd4d5093236382f5bc9f2cec
dsnimshchikov <dsnimshchikov@gmail.com>
2021-08-21 03:26:39 +0300

    Refactoring BuildTreeFromStringRecursive extract method getLeftTreeEndPosition

--- src/main/java/com/dsnimshchikov/algoritms/tree/BuildTreeFromStringRecursive.java
+++ src/main/java/com/dsnimshchikov/algoritms/tree/BuildTreeFromStringRecursive.java
@@ -3,8 +3,6 @@ package com.dsnimshchikov.algoritms.tree;
 public class BuildTreeFromStringRecursive extends AbstractTree {
 
     TreeNode<String> makeTree(String input) {
-        int breaksSum = 1;
-
         //Получаем позицию для старта
         int lengthWithOutLastBreak = input.length();
         int i = 1;
@@ -19,7 +17,27 @@ public class BuildTreeFromStringRecursive extends AbstractTree {
 
         //Получаем подстроку описывающую левое поддерево
         i++;
-        while (i < lengthWithOutLastBreak && breaksSum > 0) {
+        i = getLeftTreeEndPosition(input, i);
+        //Осталась только значимая часть левого значения. Пример: input= 6(1), далее leftValue = 1
+        if (i >= lengthWithOutLastBreak) {
+            String leftValue = input.substring(value.length() + 1, input.length() - 1);
+            root.left = new TreeNode<>(leftValue);
+            return root;
+        }
+        //Получаем подстроку описывающую правое поддерево. Пример rightTree: 5(4)(6); 4
+        if (input.length() - 1 - (i + 1) > 0) {
+            String rightTree = input.substring(i + 1, input.length() - 1);
+            root.right = makeTree(rightTree);
+        }
+        //считаем тут достаточно длинны для полноценной обработки левой части не менее 6(5)(1)
+        String leftTree = input.substring(value.length() + 1, i - 1);
+        root.left = makeTree(leftTree);
+        return root;
+    }
+
+    private int getLeftTreeEndPosition(String input, int i) {
+        int breaksSum = 1;
+        while (i < input.length() && breaksSum > 0) {
             char c = input.charAt(i);
             switch (c) {
                 case '(':
@@ -31,22 +49,9 @@ public class BuildTreeFromStringRecursive extends AbstractTree {
             }
             i++;
         }
-        //Осталась только значимая часть левого значения
-        if (i >= lengthWithOutLastBreak) {
-            String leftValue = input.substring(value.length() + 1, input.length() - 1);
-            root.left = new TreeNode<>(leftValue);
-            return root;
+        if (breaksSum != 0) {
+            throw new IllegalArgumentException("Incorrect brackets in input, check it: " + input);
         }
-        //Получаем подстроку описывающую правое поддерево
-        if (input.length() - 1 - (i + 1) > 0) {
-            String rightTree = input.substring(i + 1, input.length() - 1);
-            root.right = makeTree(rightTree);
-        }
-        //считаем тут достаточно длинны для полноценной обработки левой части не менее 6(5)(1)
-        String leftTree = input.substring(value.length() + 1, i - 1);
-        if (leftTree.length() > 0) {
-            root.left = makeTree(leftTree);
-        }
-        return root;
+        return i;
     }
 }
